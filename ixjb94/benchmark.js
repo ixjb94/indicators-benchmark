ee7a7604ad11f0a3e68591ec7ee14d6bc88d97af
IX JB <ixjb94@gmail.com>
2023-11-24 18:43:53 +0000

    benchmarkjs - remove function

--- ixjb94/benchmark.js
+++ ixjb94/benchmark.js
@@ -31,19 +31,3 @@ export class Benchmark {
         return this
     }
 }
-
-// export function benchmark(func, name, runs) {
-//     let totalTime = 0
-
-//     for (let i = 0; i < runs; i++) {
-//         const startTime = performance.now()
-
-//         func()
-
-//         const endTime = performance.now()
-//         totalTime += endTime - startTime
-//     }
-
-//     const opsPerSec = (runs / (totalTime / 1000)).toFixed(2)
-//     console.log(`${name} - ${opsPerSec} ops/sec - (${runs} runs sampled)`)
-// }
